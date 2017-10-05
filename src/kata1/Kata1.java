cfc930bca1e16b5b852823b2e5b1b683497bce65
Sandrito <Sandrito@Sandro>
2017-10-05 17:52:07 +0100

    Segunda versión de la práctica 1, Kata1.

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,10 +1,16 @@
 package kata1;
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Sandro", new Date(96,9,9));
-        System.out.println(person.getName() + " tiene " + person.getAge());  
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(1996, 10, 9);
+        
+        Person person = new Person("Sandro", date);
+        System.out.println(person.getName() + " tiene " + person.getAge()
+        + " años");
     }
     
 }
